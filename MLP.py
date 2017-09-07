76083edc45c1faf7f5d00dd6bbad4bd7178d2863
ecclesiedei <ecclesiedei@gmail.com>
2017-09-07 18:09:03 -0300

    ajuste para chamada da funcao de parser

--- MLP.py
+++ MLP.py
@@ -3,14 +3,14 @@ from sklearn.model_selection import StratifiedKFold
 import tensorflow as tf
 from random import shuffle
 import numpy as np
-from reader import readAllData
+from reader import parseData
 import time
 
 seed = 128
 rng = np.random.RandomState(seed)
 
 #clf = joblib.load('model_classifier_positive_negative.pkl') 
-X_, y_ = readAllData()
+X_, y_= parseData(isImage=True)
 
 def dense_to_one_hot(labels_dense, num_classes=9):
     """Convert class labels from scalars to one-hot vectors"""
